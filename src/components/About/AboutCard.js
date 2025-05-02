c7c327ae674da4e57d36333ee0de95c10957c78d
SaiReshmithaP <322103310199@gvpce.ac.in>
2025-05-02 16:15:50 +0530

    Initial commit

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,13 +8,12 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Sai Reshmitha Ponnaganti </span>
+            from <span className="purple"> Vizag, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently a B-Tech 3rd year student in Computer Science and Engineering at Gayatri Vidya Parishad College of Engineering.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I am a passionate web developer and I love to learn new technologies and develop projects. I am also interested in competitive programming.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -34,7 +33,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">SaiReshmitha</footer>
         </blockquote>
       </Card.Body>
     </Card>
